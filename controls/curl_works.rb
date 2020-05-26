87d3b4f782dd4d91802b8055b7d4c9d04aa16e52
Davy McAleer <dmcaleer@chef.io>
2020-05-26 10:42:16 +0100

    correcting attributes to inputs and fixing tests
    Signed-off-by: Davy McAleer <dmcaleer@chef.io>

--- controls/curl_works.rb
+++ controls/curl_works.rb
@@ -1,5 +1,7 @@
-curl_test = attribute('curl_test', default: '/bin/curl --version')
+curl_test = input('curl_test', value: '/bin/curl --version')
 
 describe bash(curl_test) do
   its('stdout') { should match /Protocols/ }
+  its('stderr') { should eq '' }
+  its('exit_status') { should eq 0 }
 end
